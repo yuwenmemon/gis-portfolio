93fdc1b075a5f5294c696df6b8c69fd01e859c7c
Yuwen Memon <yuwenis@gmail.com>
2023-11-26 19:01:42 -0800

    lowercase png

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,12 +4,12 @@ import './MainPage.css'; // Make sure to import the CSS file
 
 function MainPage() {
   const projects = [
-    { id: 1, title: "Sonoma County Hazard Scoring", imageUrl: "sonoma3.PNG", caption: "Project 1 Description", link: "/project/project1" },
-    { id: 2, title: "Sonoma Earthquake Hazard 3D View", imageUrl: "earthquake1.PNG", caption: "Project 5 Description", link: "/project/project5" },
-    { id: 3, title: "Fire Hazard in Riverside County", imageUrl: "riverside1.PNG", caption: "Project 2 Description", link: "/project/project2" },
-    { id: 4, title: "Drinking Water Contaminants Atlas", imageUrl: "atlas1.PNG", caption: "Project 3 Description", link: "/project/project3" },
-    { id: 5, title: "Cell Tower Viewshed Analysis", imageUrl: "viewshed1.PNG", caption: "Project 4 Description", link: "/project/project4" },
-    { id: 6, title: "Storybook: Film Locations of San Francisco", imageUrl: "film1.PNG", caption: "Project 6 Description", link: "/project/project6" },
+    { id: 1, title: "Sonoma County Hazard Scoring", imageUrl: "sonoma3.png", caption: "Project 1 Description", link: "/project/project1" },
+    { id: 2, title: "Sonoma Earthquake Hazard 3D View", imageUrl: "earthquake1.png", caption: "Project 5 Description", link: "/project/project5" },
+    { id: 3, title: "Fire Hazard in Riverside County", imageUrl: "riverside1.png", caption: "Project 2 Description", link: "/project/project2" },
+    { id: 4, title: "Drinking Water Contaminants Atlas", imageUrl: "atlas1.png", caption: "Project 3 Description", link: "/project/project3" },
+    { id: 5, title: "Cell Tower Viewshed Analysis", imageUrl: "viewshed1.png", caption: "Project 4 Description", link: "/project/project4" },
+    { id: 6, title: "Storybook: Film Locations of San Francisco", imageUrl: "film1.png", caption: "Project 6 Description", link: "/project/project6" },
   ];
 
   return (
