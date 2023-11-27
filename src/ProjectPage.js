3eae88fefe16e8747ce9c859db31899edf58df51
Yuwen Memon <yuwenis@gmail.com>
2023-11-26 21:36:04 -0800

    descriptions

--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -16,14 +16,16 @@ function ProjectPage() {
       <div className="project-page-content">
         <div className="project-page-description">
           {project.description}
-          <a
-            href={project.projectLink}
-            className="project-page-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            View Project <FontAwesomeIcon icon={faExternalLinkAlt} />
-          </a>
+          {project.projectLink && (
+            <a
+              href={project.projectLink}
+              className="project-page-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View Project <FontAwesomeIcon icon={faExternalLinkAlt} />
+            </a>
+          )}
         </div>
         <div className="project-page-image">
           <img src={project.imageUrl} alt={project.title} />
@@ -76,26 +78,89 @@ function getProjectDetails(projectId) {
     case 'riverside':
       return {
         title: 'Fire Hazard in Riverside County',
-        description: 'Project 2 Description...',
-        imageUrl: 'riverside3.png',
+        description: (
+          <div>
+            This project is an overview of the CAL FIRE identified hazard zones
+            in Riverside County. It identifies which neighborhoods contain a
+            high percentage of land area within a Fire Hazard Zones, as well as
+            identified individual residential parcels within the hazard zone.
+            Detailed attention is given to the neighborhoods of La Sierra Hills
+            and Alessandro Heights, which both contain a substantial portion of
+            land within the hazard zones.
+          </div>
+        ),
+        imageUrl: 'riverside4.png',
+        projectLink: 'projects/Memon_Module6_Assignment.pdf',
       };
     case 'atlas':
       return {
         title: 'Drinking Water Contaminants Atlas',
-        description: 'Project 3 Description...',
-        imageUrl: 'atlas2.png',
+        description: (
+          <div>
+            This is an atlas themed around water quality in California. It takes
+            a county-by-county view at both drinking water contaminant scores as
+            assigned by{' '}
+            <a
+              href="https://oehha.ca.gov/calenviroscreen/report/calenviroscreen-40"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              California OEHHA
+            </a>
+            , as well as the threats to groundwater posed by sites at a census
+            tract level. The atlas was compiled using ArcGIS Pro's{' '}
+            <a
+              href="https://pro.arcgis.com/en/pro-app/latest/help/layouts/map-series.htm"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Map Series
+            </a>{' '}
+            feature, which allows for the creation of a series of maps based on
+            a single layout template.
+          </div>
+        ),
+        imageUrl: 'atlas1.png',
+        projectLink: 'projects/Memon Module 4 Assignment.pdf',
       };
     case 'viewshed':
       return {
         title: 'Cell Tower Viewshed Analysis',
-        description: 'Project 4 Description...',
+        description: (
+          <div>
+            Doing a little bit of role-playing as a GIS analyst hired by
+            T-Mobile, this uses ArcGIS Pro's{' '}
+            <a
+              href="https://pro.arcgis.com/en/pro-app/latest/tool-reference/spatial-analyst/viewshed.htm"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {' '}
+              viewshed analysis
+            </a>{' '}
+            capabilities to consider three scenarios by which to extend coverage
+            for cell service within Los Angeles County.
+          </div>
+        ),
         imageUrl: 'viewshed2.png',
+        projectLink: 'projects/Memon Module 3 Assignment.pdf',
       };
     case 'sffilmstory':
       return {
         title: 'Storybook: Film Locations of San Francisco',
-        description: 'Project 6 Description...',
+        description: (
+          <div>
+            This is a fun little, film-themed storybook I created using Google
+            Earth. It is a tour of San Francisco via various famous film
+            locations within the city. Each location is accompanied by a clip of
+            the film in which it appears. The tour is meant to be a fun way to
+            explore the city, as well as a way to highlight the various great
+            films that have been shot in San Francisco over the years.
+          </div>
+        ),
         imageUrl: 'film2.png',
+        projectLink:
+          'https://earth.google.com/earth/d/1VkVogKiIN75BjANd2VLD5l5F2j7L49s-?usp=sharing',
       };
     default:
       return {
